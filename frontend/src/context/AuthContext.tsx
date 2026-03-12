a507dd54c13f9290236fe3f7c3e1be66c2aabdd3
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-03-12 19:24:19 -0400

    week4 - implementacion de custom hook

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, type ReactNode, useContext, useState } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 export interface User {
 	id: string;
@@ -15,18 +16,15 @@ interface AuthContextType {
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-	const [user, setUser] = useState<User | null>(() => {
-		const storedUser = localStorage.getItem("user");
-		return storedUser ? JSON.parse(storedUser) : null;
-	});
+	const [user, setUser] = useLocalStorage<User | null>("user", null);
 
 	const login = (userData: User) => {
 		setUser(userData);
-		localStorage.setItem("user", JSON.stringify(userData));
 	};
 
 	const logout = () => {
 		setUser(null);
+
 		localStorage.removeItem("user");
 	};
 
