34edb82fcf3c210e790b746b0cccf89d7c9538a8
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-03-12 18:26:02 -0400

    creacion del authContext

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, type ReactNode, useContext, useState } from "react";
+
+export interface User {
+	id: string;
+	name: string;
+	email: string;
+}
+
+interface AuthContextType {
+	user: User | null;
+	login: (userData: User) => void;
+	logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+	const [user, setUser] = useState<User | null>(null);
+
+	const login = (userData: User) => {
+		setUser(userData);
+	};
+
+	const logout = () => {
+		setUser(null);
+	};
+
+	return (
+		<AuthContext.Provider value={{ user, login, logout }}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
+
+export const useAuth = (): AuthContextType => {
+	const context = useContext(AuthContext);
+	if (context === undefined) {
+		throw new Error("useAuth debe usarse dentro de un AuthProvider");
+	}
+	return context;
+};
