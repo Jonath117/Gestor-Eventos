c9ffb42efdc45926f5d8136ba5f2ebbc9595b019
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-16 02:08:15 -0400

    implementacion del registro de dependencias como la coneccion a la base de datos y el uso de convencion para las tablas de postgresql

--- backend/src/Modules/Core/Core.Infrastructure/DependencyInjection.cs
+++ backend/src/Modules/Core/Core.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,30 @@
+using Core.Infrastructure.Persistence;
+
 namespace Core.Infrastructure;
 
-public class DependencyInjection
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddCoreModule(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        //services.AddCoreApplication();
+        
+        string connectionString = configuration.GetConnectionString("NeonPostgres") 
+                                  ?? throw new InvalidOperationException("No se encontró la cadena de conexión 'NeonPostgres'.");
+        
+        services.AddDbContext<CoreDbContext>(options =>
+            options.UseNpgsql(connectionString, npgsqlOptions => 
+                {
+                    npgsqlOptions.MigrationsHistoryTable("__EFMigrationsHistory", "core");
+                })
+                .UseSnakeCaseNamingConvention());
+        
+        
+        return services;
+    }
 }
