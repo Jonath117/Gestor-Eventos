1af93cafa1b9e83454ba6819ea1a934bbe58f2ff
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-16 02:15:07 -0400

    se quito la responsabilidad de mapear todas las tablas , solo se declaro el schema donde se van a crear

--- backend/src/Modules/Core/Core.Infrastructure/Persistence/CoreDbContext.cs
+++ backend/src/Modules/Core/Core.Infrastructure/Persistence/CoreDbContext.cs
@@ -1,4 +1,4 @@
-namespace Core.Infrastructure.Persistence.Configurations;
+namespace Core.Infrastructure.Persistence;
 
 using Microsoft.EntityFrameworkCore;
 using Domain.Entities;
@@ -8,20 +8,14 @@ public class CoreDbContext(DbContextOptions<CoreDbContext> options) : DbContext(
     public DbSet<Organization> Organizations { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<OrganizationUser> OrganizationUsers { get; set; }
-    
     public DbSet<Event> Events { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+        
         modelBuilder.HasDefaultSchema("core");
         
-        modelBuilder.Entity<OrganizationUser>()
-            .HasKey(ou => new { ou.OrganizationId, ou.UserId });
-        
-        modelBuilder.Entity<Event>()
-            .HasOne(e => e.Organization)
-            .WithMany(o => o.Events)
-            .HasForeignKey(e => e.OrganizationId);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CoreDbContext).Assembly);
     }
 }
