3d7277127015e506763c312aeec84d9ff3438549
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-21 04:40:12 -0400

    I implemented the pattern to have a better dependencies managment registering services like the connection and the UseSnakeNamingConvention

---
+++ backend/src/Modules/Payment/Payment.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+namespace Payment.Infrastructure;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Database;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPaymentInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        string connectionString = configuration.GetConnectionString("NeonPostgres") 
+                                  ?? throw new InvalidOperationException("No se encontro la cadena de conexión 'NeonPostgres'.");
+        
+        services.AddDbContext<PaymentDbContext>(options =>
+            options.UseNpgsql(connectionString, npgsqlOptions => 
+                {
+                    npgsqlOptions.MigrationsHistoryTable("__EFMigrationsHistory", "payment");
+                })
+                .UseSnakeCaseNamingConvention());
+        return services;
+    }
+}
