6ca70d3fb3070db43964a5d2a0c167f070763863
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-21 17:44:00 -0400

    api: rename controller routes to lowercase naming convention

--- backend/src/Modules/Core/Core.Presentation/Controllers/OrganizationsController.cs
+++ backend/src/Modules/Core/Core.Presentation/Controllers/OrganizationsController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Core.Presentation.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/organizations")]
 [Authorize]
 public class OrganizationsController(IMediator mediator) : ControllerBase
 {
