33bdbadbd962db47b8b9f0efeeae98581d0ffa6f
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-21 04:38:49 -0400

    it was implemented the factory that make the job of create a copy of PaymentDbContext for works in Design-Time

---
+++ backend/src/Modules/Payment/Payment.Infrastructure/Database/PaymentDbContextFactory.cs
@@ -0,0 +1,31 @@
+namespace Payment.Infrastructure.Database;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+public class PaymentDbContextFactory : IDesignTimeDbContextFactory<PaymentDbContext>
+{
+    public PaymentDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Development.json", optional: false)
+            .AddUserSecrets<PaymentDbContextFactory>() 
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<PaymentDbContext>();
+        
+        string connectionString = configuration.GetConnectionString("NeonPostgres")
+                                  ?? throw new InvalidOperationException("No se encontró la cadena de conexión.");
+
+        optionsBuilder.UseNpgsql(connectionString, npgsqlOptions =>
+            {
+                npgsqlOptions.MigrationsHistoryTable("__EFMigrationsHistory", "payment");
+            })
+            .UseSnakeCaseNamingConvention();
+
+        return new PaymentDbContext(optionsBuilder.Options);
+    }
+}
