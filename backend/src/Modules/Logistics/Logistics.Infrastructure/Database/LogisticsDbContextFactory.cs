3bd6fc4df6420bd7f7adf1e85e464f7c398d2ed6
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-21 02:59:06 -0400

    it was implemented the factory that make the job of create a copy of RegistrationDbContext for works in Design-Time

---
+++ backend/src/Modules/Logistics/Logistics.Infrastructure/Database/LogisticsDbContextFactory.cs
@@ -0,0 +1,31 @@
+namespace Logistics.Infrastructure.Database;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+public class LogisticsDbContextFactory : IDesignTimeDbContextFactory<LogisticsDbContext>
+{
+    public LogisticsDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Development.json", optional: false)
+            .AddUserSecrets<LogisticsDbContextFactory>() 
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<LogisticsDbContext>();
+        
+        string connectionString = configuration.GetConnectionString("NeonPostgres")
+                                  ?? throw new InvalidOperationException("No se encontró la cadena de conexión.");
+
+        optionsBuilder.UseNpgsql(connectionString, npgsqlOptions =>
+            {
+                npgsqlOptions.MigrationsHistoryTable("__EFMigrationsHistory", "logistics");
+            })
+            .UseSnakeCaseNamingConvention();
+
+        return new LogisticsDbContext(optionsBuilder.Options);
+    }
+}
