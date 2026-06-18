13223e04333d92144151d34df2f33a4d585cc82a
evertms <evert23122004@gmail.com>
2026-06-18 01:53:37 -0400

    feat: implement firebase rewrite proxy to cloud run and clean ci/cd env vars

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Instanciamos el cliente de axios usando la variable de entorno
 export const api = axios.create({
-	baseURL: import.meta.env.VITE_API_URL,
+	baseURL: "/api",
 	headers: {
 		"Content-Type": "application/json",
 	},
