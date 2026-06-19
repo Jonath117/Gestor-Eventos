da403323f104008cb618ffce945a26f6678a4dbb
evertms <evert23122004@gmail.com>
2026-06-19 16:58:12 -0400

    hotfix: register Pub/Sub publisher client in DI container

--- backend/src/Modules/Registration/Registration.Infrastructure/DependencyInjection.cs
+++ backend/src/Modules/Registration/Registration.Infrastructure/DependencyInjection.cs
@@ -1,10 +1,15 @@
+using Google.Api.Gax;
+using Google.Cloud.PubSub.V1;
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 using Registration.Application;
 using Registration.Application.Interfaces;
 using Registration.Infrastructure.Database;
+using Registration.Infrastructure.Messaging;
 
 namespace Registration.Infrastructure;
 
@@ -33,6 +38,33 @@ public static class DependencyInjection
         services.AddScoped<IRegistrationDbContext>(provider =>
             provider.GetRequiredService<RegistrationDbContext>());
 
+        RegisterPubSub(services, configuration);
+
         return services;
+    }
+
+    private static void RegisterPubSub(IServiceCollection services, IConfiguration configuration)
+    {
+        var projectId = configuration["PubSub:ProjectId"];
+        var topicId = configuration["PubSub:TopicId"];
+        var emulatorHost = configuration["PubSub:EmulatorHost"];
+
+        if (string.IsNullOrEmpty(projectId) || string.IsNullOrEmpty(topicId))
+        {
+            services.AddSingleton<IOtpMessagePublisher, NoOpOtpMessagePublisher>();
+            return;
+        }
+
+        var topicName = TopicName.FromProjectTopic(projectId, topicId);
+
+        if (!string.IsNullOrEmpty(emulatorHost))
+        {
+            Environment.SetEnvironmentVariable("PUBSUB_EMULATOR_HOST", emulatorHost);
+        }
+
+        services.AddSingleton(_ =>
+            PublisherClient.CreateAsync(topicName).GetAwaiter().GetResult());
+
+        services.AddSingleton<IOtpMessagePublisher, PubSubOtpMessagePublisher>();
     }
 }
